cedbd1434ecbdd1a77a8a3f68fdc898aa5934bd3
adityaa002 <aaeaditya02@gmail.com>
2025-01-31 15:47:01 +0530

    done

---
+++ core-java/src/com/rays/basics/oops/TestPerson.java
@@ -0,0 +1,16 @@
+package com.rays.basics.oops;
+
+import java.text.ParseException;
+
+public class TestPerson {
+	public static void main(String[] args) throws ParseException {
+
+		Person p = new Person();
+		p.setDate("26/07/2002");
+		System.out.println(p.getDate());
+
+		p.setName("Aditya");
+		System.out.println(p.getName());
+
+	}
+}
