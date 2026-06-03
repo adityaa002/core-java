7257914f5d786981233b7eb116e12e1ac6c633da
adityaa002 <aaeaditya02@gmail.com>
2026-06-03 18:59:41 +0530

    done

--- interviews/core-java/src/com/strings/StringBufferMethods.java
+++ interviews/core-java/src/com/strings/StringBufferMethods.java
@@ -2,9 +2,15 @@ package com.strings;
 
 public class StringBufferMethods {
 	public static void main(String[] args) {
-		StringBuffer sb = new StringBuffer("aditya");
+		StringBuffer sb = new StringBuffer("adityaaaa");
 		
 		System.out.println(sb.capacity());
+		
+		StringBuffer sb1 = new StringBuffer(5);
+		sb1.append("adityaa");
+		System.out.println(sb1);
+		System.out.println(sb1.capacity());
+
 	}
 
 }
